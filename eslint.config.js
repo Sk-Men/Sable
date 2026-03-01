533d758b01c0120351f5f1ea4c65d0670796cb68
hazre <mail@haz.re>
2026-03-01 13:41:33 +0100

    chore(lint): simplify eslint config and bring back airbnb rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,82 +1,78 @@
-import js from '@eslint/js';
+import path from 'node:path';
+
 import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
 import { defineConfig } from 'eslint/config';
-import { fileURLToPath } from 'node:url';
-import css from '@eslint/css';
-import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
-import tseslint from 'typescript-eslint';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
-import { createNodeResolver, importX } from 'eslint-plugin-import-x';
-import unusedImports from 'eslint-plugin-unused-imports';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import { helpers } from 'eslint-config-airbnb-extended';
+import { configs } from 'eslint-config-airbnb-extended/legacy';
+import { rules as prettierConfigRules } from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
 
-const sourceFiles = ['src/**/*.{js,jsx,ts,tsx,mjs,cjs,mts,cts}'];
-const tsSourceFiles = ['src/**/*.{ts,tsx,mts,cts}'];
-const jsSourceFiles = ['src/**/*.{js,jsx,mjs,cjs}'];
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
-const scopedRecommendedTsConfigs = tseslint.configs.recommended.map((config) =>
-  config.files ? config : { ...config, files: sourceFiles }
-);
-const scopedRecommendedTypeCheckedTsConfigs = tseslint.configs.recommendedTypeChecked.map(
-  (config) => (config.files ? config : { ...config, files: sourceFiles })
-);
+const gitignorePath = path.resolve('.', '.gitignore');
+const { jsFiles, tsFiles } = helpers.extensions;
 
-export default defineConfig(
-  includeIgnoreFile(gitignorePath),
+const jsConfig = defineConfig([
+  // ESLint recommended config
   {
-    files: ['**/*.css'],
-    plugins: { css },
-    language: 'css/css',
-    rules: css.configs.recommended.rules,
+    name: 'js/config',
+    ...js.configs.recommended,
   },
+]);
+
+const reactConfig = defineConfig([
+  // Airbnb React recommended config
+  ...configs.react.recommended,
+  // Airbnb React hooks config
+  ...configs.react.hooks,
+]);
+
+const typescriptConfig = defineConfig([
+  // Airbnb React TypeScript config
+  ...configs.react.typescript,
+]);
+
+const prettierConfig = defineConfig([
+  // Prettier plugin
   {
-    files: sourceFiles,
+    name: 'prettier/plugin/config',
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
+  // Prettier config
+  {
+    name: 'prettier/config',
+    rules: {
+      ...prettierConfigRules,
+      'prettier/prettier': 'error',
+    },
+  },
+]);
+
+const projectOverrides = defineConfig([
+  {
+    name: 'project/legacy-rule-overrides',
+    files: [...jsFiles, ...tsFiles],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
       globals: {
         JSX: 'readonly',
+        ...globals.browser,
       },
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-      'import-x/resolver-next': [
-        createTypeScriptImportResolver({
-          project: './tsconfig.json',
-        }),
-        createNodeResolver(),
-      ],
-    },
-  },
-  { ...js.configs.recommended, files: sourceFiles },
-  ...scopedRecommendedTsConfigs,
-  ...scopedRecommendedTypeCheckedTsConfigs,
-  { ...importX.flatConfigs.recommended, files: sourceFiles },
-  { ...importX.flatConfigs.typescript, files: tsSourceFiles },
-  { ...react.configs.flat.recommended, files: sourceFiles },
-  { ...react.configs.flat['jsx-runtime'], files: sourceFiles },
-  { ...jsxA11y.flatConfigs.recommended, files: sourceFiles },
-  { ...reactHooks.configs.flat.recommended, files: sourceFiles },
-  { ...tseslint.configs.disableTypeChecked, files: jsSourceFiles },
-  { ...eslintConfigPrettier, files: sourceFiles },
-  {
-    files: sourceFiles,
-    plugins: {
-      'unused-imports': unusedImports,
     },
     rules: {
       'linebreak-style': 'off',
-      'no-unused-vars': 'off',
       'no-underscore-dangle': 'off',
       'no-shadow': 'off',
+      'import/prefer-default-export': 'off',
+      'import/extensions': 'off',
+      'import/no-unresolved': 'off',
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: true,
+        },
+      ],
       'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
       'react/jsx-filename-extension': [
         'error',
@@ -86,23 +82,26 @@ export default defineConfig(
       ],
       'react/require-default-props': 'off',
       'react/jsx-props-no-spreading': 'off',
+      'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'error',
-      'import-x/no-extraneous-dependencies': [
-        'error',
-        {
-          devDependencies: true,
-        },
-      ],
-      '@typescript-eslint/no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': 'error',
-      '@typescript-eslint/no-shadow': 'error',
     },
   },
   {
-    files: tsSourceFiles,
+    name: 'project/typescript-legacy-rule-overrides',
+    files: tsFiles,
     rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-shadow': 'error',
       'no-undef': 'off',
     },
-  }
-);
+  },
+]);
+
+export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+  ...jsConfig,
+  ...reactConfig,
+  ...typescriptConfig,
+  ...prettierConfig,
+  ...projectOverrides,
+]);
