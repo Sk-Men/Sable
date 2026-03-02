c970dc3271ea1dbef36041dafbc5322e213d9d5b
7w1 <noreply@sl.sable.moe>
2026-03-02 14:49:47 -0600

    test push

--- src/app/hooks/useClientConfig.ts
+++ src/app/hooks/useClientConfig.ts
@@ -12,6 +12,7 @@ export type ClientConfig = {
   elementCallUrl?: string;
 
   disableAccountSwitcher?: boolean;
+  matrixToBaseUrl?: string;
 
   featuredCommunities?: {
     openAsDefault?: boolean;
