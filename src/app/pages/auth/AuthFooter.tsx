18d5fdf078506eceeb3d894b0d62bf627be1b58a
7w1 <noreply@sl.sable.moe>
2026-03-03 12:30:06 -0600

    bump version

--- src/app/pages/auth/AuthFooter.tsx
+++ src/app/pages/auth/AuthFooter.tsx
@@ -14,7 +14,7 @@ export function AuthFooter() {
         target="_blank"
         rel="noreferrer"
       >
-        v1.3.1
+        v1.3.2
       </Text>
       <Text as="a" size="T300" href="https://twitter.com/cinnyapp" target="_blank" rel="noreferrer">
         Twitter
