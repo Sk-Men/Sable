6525e8db28d619a98a30cb0a9308f5472b70e908
Rye <git@itsrye.dev>
2026-03-08 18:07:18 +0100

    add HTML escape and unescape utility functions

---
+++ src/app/utils/transformation.ts
@@ -0,0 +1,19 @@
+// some common transformations
+
+export function escapeHTML(str: string) {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
+
+export function unescapeHTML(str: string) {
+  return str
+    .replace(/&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'");
+}
